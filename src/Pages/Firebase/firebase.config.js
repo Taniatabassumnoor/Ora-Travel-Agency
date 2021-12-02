40c7357425c3c79900637efa7ebb2f20970c7fcf
Taniatabassumnoor <taniatabassumnoor866@gmail.com>
2021-12-02 20:33:08 +0600

    updated

--- src/Pages/Firebase/firebase.config.js
+++ src/Pages/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDKP3Ueo4vXDR0Pv8dmX0LrwVWPg87HB5c",
-    authDomain: "travel-and-tourism-8cce5.firebaseapp.com",
-    projectId: "travel-and-tourism-8cce5",
-    storageBucket: "travel-and-tourism-8cce5.appspot.com",
-    messagingSenderId: "981841329354",
-    appId: "1:981841329354:web:496123e5490a0e8b960833"
-  };
-export default firebaseConfig;  
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+export default firebaseConfig;
